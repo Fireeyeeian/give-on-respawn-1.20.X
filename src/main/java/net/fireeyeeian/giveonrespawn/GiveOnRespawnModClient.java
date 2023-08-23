18cc526d64ab610d274de4de22d0aa1f17c2adc0
FireEyeEian <fire.eye.eian@gmail.com>
2023-08-23 18:50:48 -0500

    Initial commit

---
+++ src/main/java/net/fireeyeeian/giveonrespawn/GiveOnRespawnModClient.java
@@ -0,0 +1,10 @@
+package net.fireeyeeian.giveonrespawn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GiveOnRespawnModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
